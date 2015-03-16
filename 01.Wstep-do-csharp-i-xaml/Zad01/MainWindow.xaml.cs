9677e8b4d4b9613b70587ac63e09c11ff6954ab8
jwasik <jalewa@outlook.com>
2015-03-17 00:43:54 +0100

    Dodanie 01/Zad01.

---
+++ 01.Wstep-do-csharp-i-xaml/Zad01/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="KUL.NET">
+//   Created by Jarosław Wąsik
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Zad01
+{
+    using System.Windows;
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PokazKomunikat(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Witaj świecie", "Pierwszy program");
+        }
+    }
+}
